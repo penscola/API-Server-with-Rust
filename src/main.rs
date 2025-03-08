fe13e2bdf745ea2d7203ef51008e61f4fcbd5278
penscola <penscolashackletonfelix@gmail.com>
2025-03-08 17:55:34 +0300

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("localhost", 5001))?
+        .run()
+        .await
 }
