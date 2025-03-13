e23ef4021f705c705e1ae13ca4605a450649cb9b
penscola <penscolashackletonfelix@gmail.com>
2025-03-13 14:18:30 +0300

    build(models): Building database models

---
+++ models/dog_model.rs
@@ -0,0 +1,34 @@
+use std::time::SystemTime;
+use mongodb::bson::{oid::ObjectId, DateTime};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Dog {
+    pub _id: ObjectId,
+    pub owner: ObjectId,
+    pub name: Option<String>,
+    pub age: Option<u8>,
+    pub breed: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DogRequest {
+    pub owner: String,
+    pub name: Option<String>,
+    pub age: Option<u8>,
+    pub breed: Option<String>,
+}
+
+impl TryFrom<DogRequest> for Dog {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(item: DogRequest) -> Result<Self, Self::Error> {
+        Ok(Self {
+            _id: ObjectId::new(),
+            owner: ObjectId::parse_str(&item.owner).expect("Failed tp"),
+            name: item.name,
+            age: item.age,
+            breed: item.breed,
+        })
+    }
+}
---
+++ models/owner_model.rs
@@ -0,0 +1,34 @@
+use std::time::SystemTime;
+use mongodb::bson::{oid::ObjectId, DateTime};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Owner {
+    pub _id: ObjectId,
+    pub name: String,
+    pub email: String,
+    pub phone: String,
+    pub address: String,
+}
+
+#[derive(Debug, Deserialize, SerializBookingRequeste)]
+pub struct OwnerRequest {
+    pub name: String,
+    pub email: String,
+    pub phone: String,
+    pub address: String,
+}
+
+impl TryFrom<OwnerRequest> for Owner {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(item: OwnerRequest) -> Result<Self, Self::Error> {
+        Ok(Self {
+            _id: ObjectId::new(),
+            name: item.name,
+            email: item.email,
+            phone: item.phone,
+            address: item.address,
+        })
+    }
+}
